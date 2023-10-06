4a2041ad580d1473d12e57dd871bb04620ef4385
pedroquintao <pedro.h.quintao@hotmail.com>
2023-10-05 23:40:24 -0300

    Voltei atrás com a criação do componente EditButton separado para o botao de edit

--- src/components/TaskList/Task/TaskOptions/index.jsx
+++ src/components/TaskList/Task/TaskOptions/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './TaskOptions.module.scss'
-import EditButton from './EditButton';
+// import EditButton from './EditButton';
 import { useState } from "react";
+import {AiFillEdit, AiOutlineCheck, AiOutlineClose } from 'react-icons/ai'
 import {GoKebabHorizontal} from 'react-icons/go'
 import {RiDeleteBin6Line} from 'react-icons/ri'
 
@@ -11,40 +12,57 @@ export default function TaskOptions({__id}) {
 
     const toggleVisibility = () => {setVisibility(!visibility);}
 
-    const deleteTask = async() => {
-        const requestOptions = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+//! A função delete task deve pertencer a um futuro componente que represente o botão de deletar task
+    // const deleteTask = async() => {
+    //     const requestOptions = {
+    //         method: 'DELETE',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }
 
-        const taskUrl = 'http://localhost:8080/task/'
+    //     const taskUrl = 'http://localhost:8080/task/'
 
-        try {
-            const response = await fetch(taskUrl + `${__id}`, requestOptions)
-            if(!response.ok) {throw new Error('Erro ao deletar a atividade');}
-        }
+    //     try {
+    //         const response = await fetch(taskUrl + `${__id}`, requestOptions)
+    //         if(!response.ok) {throw new Error('Erro ao deletar a atividade');}
+    //     }
         
-        catch(error) {
-                  console.error('Erro:', error);
-        }
-    }
+    //     catch(error) {
+    //               console.error('Erro:', error);
+    //     }
+    // }
 
-    
+    const [isEditModeOn, setIsClicked] = useState(false);
+
+    const toggleEditMode = () => {console.log('%cindex.jsx line:37 isEditModeOn', 'color: #007acc;', isEditModeOn);setIsClicked(!isEditModeOn);}
 
     return (
         <div className={styles.task_options}>
             <GoKebabHorizontal onClick={toggleVisibility}/>
 
             {visibility && (
-            <div className={styles.task_options__menu}>
-                <EditButton />
-                
-                <div className={styles.task_options__menu__deleteBtn} >
-                    <RiDeleteBin6Line onClick={deleteTask} size={24} />
+                <div> 
+                    {!isEditModeOn? (
+                        <div className={styles.task_options__menu}>
+                            <AiFillEdit className={styles.task_options__menu__editBtn} size={24} onClick={toggleEditMode} />
+                            <RiDeleteBin6Line className={styles.task_options__menu__deleteBtn} size={24} onClick={toggleEditMode} />
+                        </div>
+                    ) : (
+                        <div className={styles.task_options__menu}>
+                            <AiOutlineCheck size={24} onClick={toggleEditMode}/>
+                            <AiOutlineClose size={24} onClick={toggleEditMode}/>
+                        </div>
+                        )}
                 </div>
-            </div>
+                // <div className={styles.task_options__menu}>
+                //     <EditButton isEditModeOn={ isEditModeOn } toggleEditMode={toggleEditMode}/>
+                    
+                //     {!isEditModeOn && <div className={styles.task_options__menu__deleteBtn} >
+                //         <RiDeleteBin6Line //onClick={ deleteTask } 
+                //         size={24} />
+                //     </div>}
+                // </div>
                 )
             }
         </div>
